1f4ab81ef363e07d8356764ab48b3d9c1a5f1236
Alameyo <alameyo@users.noreply.github.com>
2019-04-14 11:44:14 +0200

    Fix left side view.

--- src/main/kotlin/org/alameyo/flame/views/home/roster/LeftSideView.kt
+++ src/main/kotlin/org/alameyo/flame/views/home/roster/LeftSideView.kt
@@ -1,23 +1,20 @@
 package org.alameyo.flame.views.home.roster
 
 import javafx.scene.control.TabPane
-import org.alameyo.flame.controllers.chat.ChatAreaController
-import org.alameyo.flame.controllers.FlameController
-import org.alameyo.flame.css.FlameStyle
-import org.alameyo.flame.models.FlameRosterEntry
-import tornadofx.*
+import tornadofx.View
+import tornadofx.tab
+import tornadofx.tabpane
 
 class LeftSideView : View() {
 
     private val rosterView: RosterView by inject()
-    private val multiUserChatView: MultiUserChatView by inject()
     override val root = tabpane {
         tab("Friends") {
             content = rosterView.root
         }
 
         tab("Rooms") {
-            content = multiUserChatView.root
+
         }
 
         tabClosingPolicy = TabPane.TabClosingPolicy.UNAVAILABLE
